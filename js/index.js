f412de7323d0be29a7e81f76c9d508f6a7ef17a9
nchen2211 <nchrysilla@gmail.com>
2016-04-07 20:47:40 -0700

    js for index.html

---
+++ js/index.js
@@ -0,0 +1,50 @@
+var main_topic;
+//Connecting to ROS
+window.onload = setup;
+
+function setup(){
+	
+
+	//Make sure to change the IP
+	var ros = new ROSLIB.Ros({
+		//Use the localhost one if running in on the laptop only
+		//Use the local IP if accessing it from tablet
+		
+		url: 'ws://192.168.0.21:9090'
+		//url: 'ws://192.168.5.179:9090'
+		//url: 'ws://localhost:9090'
+	});
+
+	ros.on('connection',function(){
+		console.log('Connecting to websocket server.');
+	});
+
+	ros.on('error',function(error){
+		console.log('Error connecting to websocket server: ', error);
+	});
+
+	ros.on('close',function(){
+		console.log('Connection to websocket closed.');
+	});
+
+	main_topic = new ROSLIB.Topic({
+		ros: ros,
+		name: '/Game_MAKI',
+		messageType: 'std_msgs/String'
+
+	});
+
+	start_index();
+
+}
+
+function start_index() {
+	
+	var message = new ROSLIB.Message({
+		data: "On main menu"
+	});
+
+	main_topic.publish(message);
+}
+
+
